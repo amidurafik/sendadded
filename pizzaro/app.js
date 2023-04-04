470d05daa58687f796bcfcf10ebc517ef6868aaf
amidurafik <129888410+amidurafik@users.noreply.github.com>
2023-04-04 11:22:29 -0700

    Add files via upload

---
+++ pizzaro/app.js
@@ -0,0 +1,16 @@
+function validate()
+{
+let email=document.getElementById('email').value;
+let password=document.getElementById('password').value;
+
+if (email ='webdev' && password ='learn')
+
+{alert('login sucessfuly!');}
+
+else{
+alert('login failed');
+
+}
+
+}
+
